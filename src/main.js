f4786409f574a7c9809f76c49b4d0d28d7d6fe75
u7663394 <u7663394@anu.edu.au>
2026-03-21 14:27:20 +1100

    Set theme color and font

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "@/utils/element-ui";
+import "./styles/index.scss";
 
 Vue.config.productionTip = false;
 
