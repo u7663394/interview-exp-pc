c044936eacdd7f36002d70b75c929ef7598d5229
u7663394 <u7663394@anu.edu.au>
2026-03-21 15:01:43 +1100

    Implement top-level and nested routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,25 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Login from "@/views/login";
+import Layout from "@/views/layout";
+import Dashboard from "@/views/dashboard";
+import Article from "@/views/article";
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  routes: [],
+  routes: [
+    { path: "/login", component: Login },
+    {
+      path: "/",
+      component: Layout,
+      redirect: "/dashboard",
+      children: [
+        { path: "/dashboard", component: Dashboard },
+        { path: "/article", component: Article },
+      ],
+    },
+  ],
 });
 
 export default router;
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
+    <header>头部</header>
+    <aside>侧边栏</aside>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "LayoutIndex",
+};
 </script>
 
 <style></style>
