a61fe6679b2c3386328c7e8aecf005bd10b8d38e
u7663394 <u7663394@anu.edu.au>
2026-03-21 19:48:35 +1100

    Implement login page static layout and styles

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>我是一级登录</div>
+  <div class="login-page">
+    <el-card class="el-card">
+      <template #header>面经运营后台</template>
+      <el-form>
+        <el-form-item label="用户名: ">
+          <el-input placeholder="请输入用户名" />
+        </el-form-item>
+        <el-form-item label="密码: ">
+          <el-input placeholder="请输入密码" />
+        </el-form-item>
+        <el-form-item class="tc">
+          <el-button type="primary">登录</el-button>
+          <el-button>重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -8,4 +24,29 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.login-page {
+  min-height: 100vh;
+  background: url(@/assets/login-bg.svg) no-repeat center / cover;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  .el-card {
+    width: 420px;
+    ::v-deep .el-card__header {
+      height: 80px;
+      background: rgba(114, 124, 245, 1);
+      text-align: center;
+      line-height: 40px;
+      color: #fff;
+      font-size: 18px;
+    }
+  }
+  .el-form {
+    padding: 0 20px;
+  }
+  .tc {
+    text-align: center;
+  }
+}
+</style>
